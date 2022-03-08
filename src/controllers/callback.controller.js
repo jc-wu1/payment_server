e3816c65c12c6d27a7b42ec32a2613aa76751791
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2022-03-08 16:24:38 +0700

    better currency format

--- src/controllers/callback.controller.js
+++ src/controllers/callback.controller.js
@@ -11,22 +11,17 @@ const callbackNotification = catchAsync(async (req, res) => {
         let unformattedAmount = req.body.amount;
         let isVa = req.body.callback_virtual_account_id ? true : false;
         let bankCode = isVa ? req.body.bank_code : 'null';
+        let retailName = isVa ? 'null' : req.body.retail_outlet_name
         let name = req.body.external_id;
 
-        var formatter = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'IDR',
-            // These options are needed to round to whole numbers if that's what you want.
-            //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-            //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-        });
-        
+        var formatter = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' });
+
         let amount = formatter.format(unformattedAmount); /* $2,500.00 */
 
         if (req.body.transaction_timestamp) {
             const payload = {
                 'title': `Pembayaran ${name} diterima!`,
-                'body': `Pembayaran senilai ${amount} dengan ${isVa ? `menggunakan virtual account ${bankCode}` : 'menggunakan retail'} berhasil`
+                'body': `Pembayaran senilai ${amount} dengan ${isVa ? `menggunakan virtual account ${bankCode}` : `menggunakan ${retailName}`} berhasil`
             }
             sendNotification(payload);
         }
